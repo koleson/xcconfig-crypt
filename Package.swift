f17543e7f1295f06296a96ad49ded03cb276c5d3
Kiel Oleson <kiel.oleson@thriveglobal.com>
2020-11-30 17:33:10 -0800

    add dependencies

--- Package.swift
+++ Package.swift
@@ -5,16 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "xcconfig-crypt",
+    platforms: [.macOS(.v10_15)],       // swift-crypto requires 10.15+
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+	.package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
+	.package(url: "https://github.com/apple/swift-crypto/", .upToNextMajor(from: "1.1.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "xcconfig-crypt",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Crypto", package: "swift-crypto")
+            ]),
         .testTarget(
             name: "xcconfig-cryptTests",
             dependencies: ["xcconfig-crypt"]),
